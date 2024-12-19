11c8af1fec580da90dc589d1aec80136281aa454
chirag-qacraft <chirag@qacraft.com>
2024-12-19 18:54:29 +0530

    initial

--- pages/addToCart.ts
+++ pages/addToCart.ts
@@ -9,7 +9,21 @@ export class AddToCart{
     readonly contShoppingButton : Locator;
     readonly cartLink : Locator;
     readonly verifyItemInCart : Locator;
-    readonly verifyProduct : Locator;
+    readonly verifyPrice : Locator;
+    readonly verifyQty : Locator;
+    readonly verifyTotalPrice : Locator;
+    readonly removeItemButton : Locator;
+    readonly verifyEmptyCart : Locator;
+    readonly viewCart : Locator;
+    readonly checkoutButton : Locator;
+    readonly placeOrderButton : Locator;
+    readonly cardName : Locator;
+    readonly cardNumber : Locator;
+    readonly cardCVC : Locator;
+    readonly cardExpiryMonth : Locator;
+    readonly cardExpiryYear : Locator;
+    readonly pay : Locator;
+    readonly invoice : Locator;
 
     constructor (page:Page)
     {
@@ -18,8 +32,22 @@ export class AddToCart{
         this.addToCartButton = page.locator("(//a[text()='Add to cart'])[1]");
         this.contShoppingButton = page.locator("//button[contains(text(),'Continue')]");
         this.cartLink = page.locator("//a[contains(text(),'Cart')]//parent::li");
-        this.verifyItemInCart = page.locator("//p[contains(text(),'Women')]");
-        
+        this.verifyItemInCart = page.locator("//a[contains(text(),'Blue')]");
+        this.verifyPrice = page.locator("(//p[contains(text(),'500')])[1]");
+        this.verifyQty = page.locator("//button[text()='1']");
+        this.verifyTotalPrice = page.locator("//p[@class='cart_total_price']");
+        this.removeItemButton = page.locator("//a[@class='cart_quantity_delete']");
+        this.verifyEmptyCart = page.locator("//b[contains(text(),'empty!')]");
+        this.viewCart = page.locator("//u[contains(text(),'View')]");
+        this.checkoutButton = page.locator("//a[contains(text(),'Checkout')]");
+        this.placeOrderButton = page.locator("//a[contains(text(),'Place')]");
+        this.cardName = page.locator("//input[@name='name_on_card']");
+        this.cardNumber = page.locator("//input[@name='card_number']");
+        this.cardCVC = page.locator("//input[@name='cvc']");
+        this.cardExpiryMonth = page.locator("//input[@name='expiry_month']");
+        this.cardExpiryYear = page.locator("//input[@name='expiry_year']");
+        this.pay = page.locator("//button[@id='submit']");
+        this.invoice = page.locator("//a[contains(text(),'Invoice')]");
     }
 
     async addToCartMethod()
@@ -27,11 +55,43 @@ export class AddToCart{
         await this.productLink.click();
         await this.addToCartButton.hover();
         await this.addToCartButton.click();
+        await this.page.waitForTimeout(3000);
         await this.contShoppingButton.click();
         await this.page.waitForTimeout(3000);
         await this.cartLink.click();
         await this.page.waitForTimeout(3000);
     }
 
+    async removeFromCartMethod()
+    {
+        await this.removeItemButton.click();
+    }
 
+    async checkOutMethod()
+    {
+        await this.productLink.click();
+        await this.addToCartButton.hover();
+        await this.addToCartButton.click();
+        await this.page.waitForTimeout(3000);
+        await this.contShoppingButton.click();
+        await this.cartLink.click();
+        await this.checkoutButton.click();
+        await this.page.waitForTimeout(1000);
+        await this.placeOrderButton.click();
+
+    }
+
+    async cardPaymentMethod()
+    {
+        await this.cardName.fill("Chirag");
+        await this.cardNumber.fill("1234567890");
+        await this.cardCVC.fill("311");
+        await this.cardExpiryMonth.fill("01");
+        await this.cardExpiryYear.fill("2040");
+        await this.page.waitForTimeout(1000);
+        await this.pay.click();
+        await this.page.waitForTimeout(1000);
+        await this.invoice.click();
+        await this.page.waitForTimeout(4000);
+    }
 }
--- tests/addToCart.spec.ts
+++ tests/addToCart.spec.ts
@@ -18,6 +18,26 @@ test('Add to Cart', async ({ page }) => {
   const addToCartObj = new AddToCart(page);
   await addToCartObj.addToCartMethod();
 
-  await expect(addToCartObj.verifyItemInCart).toBeVisible({ timeout: 30000 });
+  await expect(addToCartObj.verifyItemInCart).toHaveText("Blue Top");
+  await page.waitForTimeout(2000);
   
+  await expect(addToCartObj.verifyPrice).toHaveText("Rs. 500");
+  await page.waitForTimeout(2000);
+
+  await expect(addToCartObj.verifyQty).toHaveText("1");
+  await page.waitForTimeout(2000);
+
+  await expect(addToCartObj.verifyTotalPrice).toHaveText("Rs. 500");
+  await page.waitForTimeout(2000);
+
+  await addToCartObj.removeFromCartMethod();
+  await page.waitForTimeout(2000);
+
+  await expect(addToCartObj.verifyEmptyCart).toHaveText("Cart is empty!");
+
+  await addToCartObj.checkOutMethod();
+  
+  await addToCartObj.cardPaymentMethod();
+
+  console.log ("Both Test 1 & 2 are Completed... Nice Experience!!!")
 });
